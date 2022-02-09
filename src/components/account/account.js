030209165fcc044912159aaed603c5a772fdab10
RJDixon7 <rjdixon7@gmail.com>
2022-02-09 16:00:50 -0500

    updated

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -1,6 +1,26 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import * as actions from '../../actions';
 
 class Account extends Component {
+
+    componentDidMount() {
+
+        const navbarLinks = [
+            {
+                title: 'Purchase History',
+                active: false
+            },
+            {
+                title: 'Account Information',
+                active: true
+            },
+        ]
+
+        this.props.setHeaderLinks([]);
+        this.props.setNavbarLinks([navbarLinks]);
+    }
 
     renderContent() {
         return <h1> content </h1>
@@ -14,5 +34,7 @@ class Account extends Component {
         )
     }
 }
+
+Account = connect(null, actions)(Account);
 
 export default Account;
